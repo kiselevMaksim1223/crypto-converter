11f3c15063262d1abafd3f159ec10408c8be9cbf
kiselevMaksim1223 <laurkurt@gmail.com>
2024-03-18 11:53:54 +0400

    wrap main with provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { Providers } from '@common/providers.tsx'
+
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<Providers>
+			<App />
+		</Providers>
+	</React.StrictMode>
 )
