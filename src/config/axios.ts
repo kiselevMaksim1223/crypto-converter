82c5dfc91368ff8c7260aa1e8b96d0ed83c335ce
kiselevMaksim1223 <laurkurt@gmail.com>
2024-03-18 12:14:32 +0400

    replace api key in env

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -4,9 +4,7 @@ const options: CreateAxiosDefaults = {
 	baseURL: 'https://rest.coinapi.io/v1/exchangerate/',
 	headers: {
 		'Content-Type': 'application/json',
-		// 'X-CoinAPI-Key': 'C90C4D9C-8A2A-443B-BD2B-FB40FB145D11'
-		// 'X-CoinAPI-Key': 'A8610631-D19E-4205-ACAC-C425C62312EA'
-		'X-CoinAPI-Key': '1C15EC26-C755-4BF3-AF43-812D05040A9D'
+		'X-CoinAPI-Key': import.meta.env.VITE_COIN_API_KEY
 	}
 }
 
